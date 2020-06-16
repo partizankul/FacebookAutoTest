2dc9955dbc5c7d7dcb427c64267ca1413a7d7b26
Aleksey Kuleshov <Partizankul@gmail.com>
2020-06-16 22:26:24 +0300

    add test

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -15,6 +15,8 @@ public class LoginPage {
     private By loginInButton = By.xpath("//*[@id=\"loginbutton\"]");
     private By forgotPassLink = By.xpath("//*[text()=\"Забыли аккаунт?\"]");
     private By createAkkButton = By.xpath("//div/a[@role=\"button\"]");
+    private By passError = By.xpath("//*[text()=\"Вы ввели неверный пароль. \"]");
+
 
     public String getMailErrorText() {
         return driver.findElement(mailError).getText();
@@ -31,13 +33,24 @@ public class LoginPage {
         driver.findElement(loginInButton).click();
         return new LoginPage(driver);
     }
+
+    public LoginPage registred (String mail, String pass){
+        this.typeMail(mail);
+        this.typePass(pass);
+        this.clickLoginInButton();
+        return new LoginPage(driver);
+    }
+
     public LoginPage clickForgotPassLink() {
         driver.findElement(forgotPassLink).click();
         return new LoginPage(driver);
+    }
+    public String passErrorText(){
+        return driver.findElement(passError).getText();
     }
     public LoginPage clickCreateAkkButton() {
         driver.findElement(createAkkButton).click();
         return new LoginPage(driver);
     }
-}
 
+}
--- src/test/java/LoginPageTest.java
+++ src/test/java/LoginPageTest.java
@@ -1,2 +1,42 @@
+import Utilites.Driver;
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class LoginPageTest {
+
+    private LoginPage loginPage;
+    private Logger logger;
+
+    @Before
+    public void setUp() {
+        logger = Logger.getLogger("new logger");
+        Driver.getDriver().get("https://www.facebook.com/login");
+        loginPage = new LoginPage(Driver.getDriver());
+
+    }
+    @Test
+    public void registredMailTest(){
+        logger.info("start LoginPageTest test registredMailTest");
+        loginPage.registred("", "test");
+        logger.info("loginPage.getMailErrorText() = " + loginPage.getMailErrorText());
+        Assert.assertEquals("Эл. адрес или номер телефона, который вы указали, не соответствует ни одному аккаунту. Зарегистрируйте аккаунт.", loginPage.getMailErrorText());
+        logger.info("finish LoginPageTest test registredMailTest");
+    }
+
+    @Test
+    public void registredPassTest(){
+        logger.info("start LoginPageTest test registredPassTest");
+        loginPage.registred("Test@mail.ru", "test");
+        Assert.assertEquals("Вы ввели неверный пароль. Забыли пароль?", loginPage.passErrorText());
+        logger.info("finish LoginPageTest test registredPassTest");
+    }
+
+    /*@After
+    public void tearDown(){
+        Driver.getDriver().quit();
+    }
+*/
 }
